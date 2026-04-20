5e7fa05c0eee97cd26027df29e13c29bb4279d5c
mickzhan <mickzhan@student.42.fr>
2026-04-20 16:49:18 +0200

    ajout map_checker

--- src/cub3d.h
+++ src/cub3d.h
@@ -6,20 +6,20 @@
 /*   By: mickzhan <mickzhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 15:24:18 by mickzhan          #+#    #+#             */
-/*   Updated: 2026/04/20 16:27:54 by mickzhan         ###   ########.fr       */
+/*   Updated: 2026/04/20 16:37:03 by mickzhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <math.h>
-#include <stdlib.h>
-#include <fcntl.h>
-
-
 # include "parsing/parsing.h"
+# include "../libft/libft.h"
+# include <fcntl.h>
+# include <math.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,16 +6,53 @@
 /*   By: mickzhan <mickzhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/17 16:50:04 by mickzhan          #+#    #+#             */
-/*   Updated: 2026/04/20 15:22:22 by mickzhan         ###   ########.fr       */
+/*   Updated: 2026/04/20 16:48:41 by mickzhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "stdio.h"
+#include "cub3d.h"
 
-int main(int ac, char **av)
+int	map_cub(char *str)
 {
-    int i;
+	int		i;
+	int		j;
+	char	*cub;
 
-    
-    return (0);
+	i = 0;
+	j = 0;
+	cub = ".cub";
+	while (str[i])
+		i++;
+	i -= 4;
+	while (str[i] == cub[j])
+	{
+		i++;
+		j++;
+	}
+	if (j == 5)
+	{
+		j = open(str, O_RDONLY);
+		close(j);
+		if (j < 0)
+			return (ft_putendl_fd("Error\nMauvais fichier", 2), 1);
+	}
+	else
+		return (ft_putendl_fd("Error\nMauvais fichier", 2), 1);
+	return (0);
+}
+
+int	error_gestion(int ac, char **av)
+{
+	if (ac != 2)
+		return (printf("Bad argument"), 1);
+	if (map_cub(av[1]) == 1)
+		return (printf("Bad map"), 1);
+	return (0);
+}
+
+int	main(int ac, char **av)
+{
+	if (error_gestion(ac, av) == 1)
+		return (1);
+	return (0);
 }
