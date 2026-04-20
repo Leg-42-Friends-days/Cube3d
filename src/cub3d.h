567211f680b7e70dd67761fe489fa30d0b731f5f
mickzhan <mickzhan@student.42.fr>
2026-04-20 16:28:29 +0200

    ajout .h

--- src/cub3d.h
+++ src/cub3d.h
@@ -6,12 +6,19 @@
 /*   By: mickzhan <mickzhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/20 15:24:18 by mickzhan          #+#    #+#             */
-/*   Updated: 2026/04/20 15:25:42 by mickzhan         ###   ########.fr       */
+/*   Updated: 2026/04/20 16:27:54 by mickzhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <math.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
 
 # include "parsing/parsing.h"
 
