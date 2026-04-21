a5c6548d420dc853cbb0ac7fb4303744761834a5
mickzhan <mickzhan@student.42.fr>
2026-04-21 16:18:40 +0200

    error map

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mickzhan <mickzhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/17 16:50:04 by mickzhan          #+#    #+#             */
-/*   Updated: 2026/04/21 12:04:57 by mickzhan         ###   ########.fr       */
+/*   Updated: 2026/04/21 16:18:11 by mickzhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,11 +50,56 @@ int	error_gestion(int ac, char **av)
 	return (0);
 }
 
-bool	read_map(t_map *map)
+int	map_len(char *map_content)
 {
-	int i;
+	int		fd;
+	int		len;
+	char	*line;
+
+	len = 0;
+	fd = open(map_content, O_RDONLY);
+	if (fd == -1)
+		return (printf("Error fd"), exit(1), 1);
+	line = get_next_line(fd);
+	if (!line)
+		return (printf("Error\n Map"), exit(1), 1);
+	while (line)
+	{
+		free(line);
+		line = get_next_line(fd);
+		len++;
+	}
+	close(fd);
+	return (len);
+}
+
+char	**read_map(char *map_content)
+{
+	int		fd;
+	int		len;
+	char	*line;
+	char	**content;
+	int		i;
 
 	i = 0;
+	len = map_len(map_content);
+	content = malloc(sizeof(char *) * (len + 1));
+	if (!content)
+		return (printf("Error\nMalloc"), exit(1), NULL);
+	fd = open(map_content, O_RDONLY);
+	if (!fd)
+		return (printf("Error\n"), exit(1), NULL);
+	line = get_next_line(fd);
+	while (line)
+	{
+		content[i] = ft_strdup(line);
+		free(line);
+		line = get_next_line(fd);
+		printf("%s", content[i]);
+		i++;
+	}
+	content[i] = NULL;
+	return (content);
 }
 
 int	main(int ac, char **av)
@@ -64,6 +109,6 @@ int	main(int ac, char **av)
 	if (error_gestion(ac, av) == 1)
 		return (1);
 	map = malloc(sizeof(t_map));
-	read_map(&map);
+	map->map = read_map(av[1]);
 	return (0);
 }
