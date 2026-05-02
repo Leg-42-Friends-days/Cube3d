fc129af1cd8395537cc6f06034423cb0af01865c
ibrouin- <ibrouin-@f6r7s8.paris.42.school>
2026-05-02 15:17:25 +0200

    suppression printf

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mickzhan <mickzhan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ibrouin- <ibrouin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/17 16:50:04 by mickzhan          #+#    #+#             */
-/*   Updated: 2026/05/02 14:56:49 by mickzhan         ###   ########.fr       */
+/*   Updated: 2026/05/02 15:16:34 by ibrouin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 int	main(int ac, char **av)
 {
 	t_global	*global;
-	int			i;
 
 	if (error_gestion(ac, av) == 1)
 		return (1);
@@ -23,15 +22,6 @@ int	main(int ac, char **av)
 	read_map(global, av[1]);
 	if (start_map(global, av[1]) == 1)
 		return (free_all(global), 1);
-	i = 0;
-	while (global->map.mapy[i])
-	{
-		printf("Map : %s", global->map.mapy[i]);
-		i++;
-	}
-	printf("\n");
-	printf("height : %d\n", global->map.height);
-	printf("width : %d\n", global->map.width);
 	free_all(global);
 	return (0);
 }
