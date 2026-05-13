04bda00139cd5a7359f8e2d07d1408f5f7251403
mickzhan <mickzhan@student.42.fr>
2026-05-13 15:13:21 +0200

    correction de la texture S conflit avec joueur donc sa sera B!

--- src/parsing/map_reader.c
+++ src/parsing/map_reader.c
@@ -6,7 +6,7 @@
 /*   By: mickzhan <mickzhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 17:57:49 by mickzhan          #+#    #+#             */
-/*   Updated: 2026/05/11 16:51:24 by mickzhan         ###   ########.fr       */
+/*   Updated: 2026/05/13 15:12:25 by mickzhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -180,7 +180,7 @@ bool	stock_checker(t_global *global)
 	int	i;
 
 	i = 0;
-	while (i < 9)
+	while (i < 8)
 	{
 		if (global->textures->stock[i] != 1)
 			return (true);
@@ -373,13 +373,13 @@ void	convert_line(t_global *global, char *line, int fd)
 				error_exit(global));
 		global->textures->start++;
 	}
-	else if (ft_strncmp(is_space(line), "S", 1) == 0)
+	else if (ft_strncmp(is_space(line), "B", 1) == 0)
 	{
-		global->textures->stock[6] = 1;
+		global->textures->stock[7] = 1;
 		global->textures->bonus[1] = 1;
-		global->textures->sprite = texture_map(is_space(line) + 1);
+		global->textures->sprite = texture_map(is_space(line) + 2);
 		if (!global->textures->sprite)
-			return (ft_printf(2, "Error\nTexture (S)\n"), free(line), close(fd),
+			return (ft_printf(2, "Error\nTexture (B)\n"), free(line), close(fd),
 				error_exit(global));
 		global->textures->start++;
 	}
@@ -444,7 +444,7 @@ void	add_bonus_map(t_global *global, char *str)
 		global->textures->stock[6] = 0;
 		global->textures->bonus[0] = 1;
 	}
-	if (ft_strncmp(str, "S", 1) == 0)
+	else if (ft_strncmp(str, "B", 1) == 0)
 	{
 		global->textures->stock[7] = 0;
 		global->textures->bonus[1] = 1;
@@ -494,8 +494,8 @@ bool	read_unique(t_global *global, char *map_content)
 		return (ft_printf(2, "Error\nInvalid map (C)\n"), true);
 	if (bonus_reader(global, map_content, "D") == 1)
 		return (ft_printf(2, "Error\nInvalid map (D)\n"), true);
-	if (bonus_reader(global, map_content, "S") == 1)
-		return (ft_printf(2, "Error\nInvalid map (S)\n"), true);
+	if (bonus_reader(global, map_content, "B") == 1)
+		return (ft_printf(2, "Error\nInvalid map (B)\n"), true);
 	return (false);
 }
 
@@ -606,7 +606,7 @@ bool	char_check(char ch, t_global *global)
 		return (true);
 	else if (global->textures->bonus[0] == 1 && ch == 'D')
 		return (true);
-	else if (global->textures->bonus[1] == 1 && ch == 'S')
+	else if (global->textures->bonus[1] == 1 && ch == 'B')
 		return (true);
 	else
 		return (false);
@@ -620,12 +620,7 @@ bool	direction_check(char ch)
 		return (false);
 }
 
-// bool	bonus_char_check(char ch, t_global *global)
-// {
-// 	if (global->textures->bonus[0] == 1 && ch == 'D')
-// 		return (true);
-// 	return (false);
-// }
+
 
 bool	error_check(char **str, t_global *global)
 {
@@ -644,8 +639,6 @@ bool	error_check(char **str, t_global *global)
 				cpt++;
 			if (char_check(str[i][j], global))
 				j++;
-			// else if (bonus_char_check(str[i][j], global))
-				// j++;
 			else
 				return (true);
 		}
@@ -857,7 +850,7 @@ bool	start_map(t_global *global, char *map_content)
 	if (!global->map.mapou)
 		return (ft_printf(2, "Error\nMap\n"), error_exit(global), true);
 	if (map_check(global) == 1)
-		return (error_exit(global), true);
+		return (printf("Error\nInvalid map\n"), error_exit(global), true);
 	if (check_mapline(global->map.mapou) == 1)
 		return (ft_printf(2, "Error\nMap\n"), error_exit(global), true);
 	global->map.height = get_height_map(map_content);
@@ -865,7 +858,7 @@ bool	start_map(t_global *global, char *map_content)
 		return (error_exit(global), true);
 	global->map.height -= global->textures->start;
 	global->map.width = get_width_map(global->map.mapou);
-	if (build_fake_map(global))
+	if (build_fake_map(global))	
 		return (error_exit(global), true);
 	if (map_flood(global) == 1)
 		return (ft_printf(2, "Error\nMap incorrect\n"), true);
