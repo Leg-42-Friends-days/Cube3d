58ce9094bc8aa720f320e7c90e02aa59bf7291df
mickzhan <mickzhan@student.42.fr>
2026-05-15 15:25:32 +0200

    norme

--- src/hook.c
+++ src/hook.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   hook.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ibrouin- <ibrouin-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mickzhan <mickzhan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/01 13:51:56 by ibrouin-          #+#    #+#             */
-/*   Updated: 2026/05/11 16:38:15 by ibrouin-         ###   ########.fr       */
+/*   Updated: 2026/05/15 15:24:35 by mickzhan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,108 +15,107 @@
 int	close_window(t_global *global)
 {
 	mlx_destroy_image(global->mlx, global->img.img);
-    mlx_destroy_image(global->mlx, global->raycast_data.north.img_ptr);
-    mlx_destroy_image(global->mlx, global->raycast_data.west.img_ptr);
-    mlx_destroy_image(global->mlx, global->raycast_data.east.img_ptr);
-    mlx_destroy_image(global->mlx, global->raycast_data.south.img_ptr);
+	mlx_destroy_image(global->mlx, global->raycast_data.north.img_ptr);
+	mlx_destroy_image(global->mlx, global->raycast_data.west.img_ptr);
+	mlx_destroy_image(global->mlx, global->raycast_data.east.img_ptr);
+	mlx_destroy_image(global->mlx, global->raycast_data.south.img_ptr);
 	mlx_destroy_window(global->mlx, global->win);
 	mlx_destroy_display(global->mlx);
 	free(global->mlx);
 	free_all(global);
-	exit (8);
+	exit(8);
 }
 
 int	close_window_hook(void *param)
 {
-	t_global    *global;
+	t_global	*global;
 
 	global = (t_global *)param;
 	mlx_destroy_image(global->mlx, global->img.img);
-    mlx_destroy_image(global->mlx, global->raycast_data.north.img_ptr);
-    mlx_destroy_image(global->mlx, global->raycast_data.west.img_ptr);
-    mlx_destroy_image(global->mlx, global->raycast_data.east.img_ptr);
-    mlx_destroy_image(global->mlx, global->raycast_data.south.img_ptr);
+	mlx_destroy_image(global->mlx, global->raycast_data.north.img_ptr);
+	mlx_destroy_image(global->mlx, global->raycast_data.west.img_ptr);
+	mlx_destroy_image(global->mlx, global->raycast_data.east.img_ptr);
+	mlx_destroy_image(global->mlx, global->raycast_data.south.img_ptr);
 	mlx_destroy_window(global->mlx, global->win);
 	mlx_destroy_display(global->mlx);
 	free(global->mlx);
 	free_all(global);
-	exit (8);
+	exit(8);
 }
 
 int	key_hook(void *param)
 {
-	t_global *global;
+	t_global	*global;
 
-    global = (t_global *)param;
-    if (global->hook.left)
-        rotate(LEFT, global);
-    if (global->hook.right)
-        rotate(RIGHT, global);
-    //if (global->hook.up)
-   //     walk(UP, global);
-    //if (global->hook.down)
-    //    walk(DOWN, global);
-    if (global->hook.w)
-        walk(W, global);
-    if (global->hook.a)
-        crab_walk(A, global);
-    if (global->hook.s)
-        walk(S, global);
-    if (global->hook.d)
-        crab_walk(D, global);
-    //raycasting(global);
-    //mlx_put_image_to_window(global->mlx, global->win,
-    //    global->img.img, 0, 0);
-    return (0);
+	global = (t_global *)param;
+	if (global->hook.left)
+		rotate(LEFT, global);
+	if (global->hook.right)
+		rotate(RIGHT, global);
+	// if (global->hook.up)
+	//     walk(UP, global);
+	// if (global->hook.down)
+	//    walk(DOWN, global);
+	if (global->hook.w)
+		walk(W, global);
+	if (global->hook.a)
+		crab_walk(A, global);
+	if (global->hook.s)
+		walk(S, global);
+	if (global->hook.d)
+		crab_walk(D, global);
+	// raycasting(global);
+	// mlx_put_image_to_window(global->mlx, global->win,
+	//    global->img.img, 0, 0);
+	return (0);
 }
 
-int press_on(int keycode, void *param)
+int	press_on(int keycode, void *param)
 {
-    t_global *global;
+	t_global	*global;
 
-    global = (t_global *)param;
-    if (keycode == ESC)
-        close_window(global);
-    if (keycode == LEFT)
-        global->hook.left = 1;
-    if (keycode == RIGHT)
-        global->hook.right = 1;
-    if (keycode == UP)
-        global->hook.up = 1;
-    if (keycode == DOWN)
-        global->hook.down = 1;
-    if (keycode == W)
-        global->hook.w = 1;
-    if (keycode == A)
-        global->hook.a = 1;
-    if (keycode == S)
-        global->hook.s = 1;
-    if (keycode == D)
-        global->hook.d = 1;
-    return (0);
+	global = (t_global *)param;
+	if (keycode == ESC)
+		close_window(global);
+	if (keycode == LEFT)
+		global->hook.left = 1;
+	if (keycode == RIGHT)
+		global->hook.right = 1;
+	if (keycode == UP)
+		global->hook.up = 1;
+	if (keycode == DOWN)
+		global->hook.down = 1;
+	if (keycode == W)
+		global->hook.w = 1;
+	if (keycode == A)
+		global->hook.a = 1;
+	if (keycode == S)
+		global->hook.s = 1;
+	if (keycode == D)
+		global->hook.d = 1;
+	return (0);
 }
 
-int press_off(int keycode, void *param)
+int	press_off(int keycode, void *param)
 {
-    t_global *global;
+	t_global	*global;
 
-    global = (t_global *)param;
-    if (keycode == LEFT)
-        global->hook.left = 0;
-    if (keycode == RIGHT)
-        global->hook.right = 0;
-    if (keycode == UP)
-        global->hook.up = 0;
-    if (keycode == DOWN)
-        global->hook.down = 0;
-    if (keycode == W)
-        global->hook.w = 0;
-    if (keycode == A)
-        global->hook.a = 0;
-    if (keycode == S)
-        global->hook.s = 0;
-    if (keycode == D)
-        global->hook.d = 0;
-    return (0);
+	global = (t_global *)param;
+	if (keycode == LEFT)
+		global->hook.left = 0;
+	if (keycode == RIGHT)
+		global->hook.right = 0;
+	if (keycode == UP)
+		global->hook.up = 0;
+	if (keycode == DOWN)
+		global->hook.down = 0;
+	if (keycode == W)
+		global->hook.w = 0;
+	if (keycode == A)
+		global->hook.a = 0;
+	if (keycode == S)
+		global->hook.s = 0;
+	if (keycode == D)
+		global->hook.d = 0;
+	return (0);
 }
-
